4c218e3d663f49c48dac2a01e27a7c2024a2076a
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-18 12:02:35 +0530

    remove

--- api/routes/connection.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router()
-
-const { countConnection } = require('../controller/connection')
-
-router.post('/connection', countConnection)
-
-module.exports = router
--- api/utils.js
+++
@@ -1,1 +0,0 @@
-var macaddress = require('macaddress')
